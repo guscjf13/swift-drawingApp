a34ef8dca9405172c6c890f0f0189c4b37758acc
guscjf13 <byunhyunchul@naver.com>
2022-07-04 15:38:28 +0900

    pod init, snapkit 추가

---
+++ swift-drawingapp/Pods/Target Support Files/Pods-Swift-drawingApp-Swift-drawingAppUITests/Pods-Swift-drawingApp-Swift-drawingAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Swift_drawingApp_Swift_drawingAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Swift_drawingApp_Swift_drawingAppUITests
+@end
---
+++ swift-drawingapp/Pods/Target Support Files/Pods-Swift-drawingAppTests/Pods-Swift-drawingAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Swift_drawingAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_Swift_drawingAppTests
+@end
